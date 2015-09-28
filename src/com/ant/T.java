1a1b9237eb64fa45b7c657d5cdc89222ade3301f
jiabangping <1355926178@qq.com>
2015-09-28 10:14:00 +0800

    first commit

---
+++ src/com/ant/T.java
@@ -0,0 +1,9 @@
+package com.ant;
+
+
+public class T {
+	public static void main(String[] args) {
+		// System.out.println(System.currentTimeMillis()/1000);
+		//现在的修订版本是 4  修改了T 和Test.java 文件， 测试提交后，回滚到4之前状态。
+	}  
+}
---
+++ src/com/ant/Test.java
@@ -0,0 +1,10 @@
+package com.ant;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+	private void f() {
+		//现在的修订版本是 4  修改了T 和Test.java 文件， 测试提交后，回滚到4之前状态。
+	}
+}
